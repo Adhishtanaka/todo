b31bc2ac65dbc96928404fcb2bd4fa97dc6b4fd6
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-04-01 22:32:02 +0530

    improve swagger functionality by adding token storing

--- src/main/java/at/todo/config/OpenAPIConfig.java
+++ src/main/java/at/todo/config/OpenAPIConfig.java
@@ -3,6 +3,8 @@ package at.todo.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +20,14 @@ public class OpenAPIConfig {
                         .description("API Documentation")
                         .contact(new Contact()
                                 .name("Adhishtanaka Thiramithu Kulasooriya")
-                                .email("Kulasoooriyaa@gmail.com")));
+                                .email("Kulasoooriyaa@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("BearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
